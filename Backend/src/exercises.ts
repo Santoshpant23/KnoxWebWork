855b5ff6363f0057e315d1fb1e24771b003cbc0f
Santosh <pantsantosh23@gmail.com>
2024-12-01 16:54:30 -0600

    Implemented exercises and student endpoints

--- Backend/src/exercises.ts
+++ Backend/src/exercises.ts
@@ -1,13 +1,150 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 // Instantiate the client
 const prisma = new PrismaClient();
 
 const exercise = Router();
 
+const SECRET = process.env.SECRETKEY || "Santosh";
+
 exercise.get("/", (req, res) => {
   res.send("Welcome to exercise endpoint");
+});
+
+exercise.get("/all-exercises", async (req: any, res: any) => {
+  try {
+    const { token, cookie } = req.body;
+    if (!token && !cookie) {
+      return res.json({
+        success: false,
+        message: "Unauthorized access",
+      });
+    }
+
+    //not doing anything for admin for now, just focusing on students
+    //somehow decoding cookie and making sure the user is authenticated to be here
+
+    //here, I will eventually make sure date of release is actually considered while showing the exercises to the students
+    const courseId = await req.body.courseId;
+    const allExercises = prisma.exercise.findMany({
+      where: {
+        courseId: courseId,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Success",
+      exercises: allExercises,
+    });
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
+});
+
+exercise.post("/add-exercise", async (req: any, res: any) => {
+  try {
+    const { token, courseId } = req.body;
+
+    if (!token || !courseId) {
+      return res.json({
+        success: false,
+        message: "Unauthorized Action",
+      });
+    }
+
+    const verifyToken = jwt.verify(token, SECRET) as { email: string };
+
+    if (!verifyToken) {
+      return res.json({
+        success: false,
+        message: "Cannot verify user",
+      });
+    }
+
+    const getOwner = prisma.owners.findFirst({
+      where: {
+        email: verifyToken.email,
+      },
+    });
+
+    if (!getOwner) {
+      return res.json({
+        success: false,
+        message: "User does not exist, please signup",
+      });
+    }
+
+    const course = prisma.course.findFirst({
+      where: {
+        id: courseId,
+        ownedBy: verifyToken.email,
+      },
+    });
+
+    if (!course) {
+      return res.json({
+        success: false,
+        message: "Course does not exist",
+      });
+    }
+
+    const { name, datePosted, dateExpires } = req.body;
+
+    if (!name || !datePosted || !dateExpires) {
+      return res.json({
+        success: false,
+        message: "Insufficient Information Provided",
+      });
+    }
+
+    /*
+    model Exercise {
+  id          Int        @id @default(autoincrement())
+  name        String     @unique
+  datePosted  DateTime
+  dateExpires DateTime
+  questions   Question[]
+  courseId    Int
+  course      Course     @relation(fields: [courseId], references: [id])
+}
+
+  */
+    const exercise = await prisma.exercise.create({
+      data: {
+        name: name,
+        datePosted: datePosted,
+        dateExpires: dateExpires,
+        courseId: courseId,
+      },
+    });
+
+    await prisma.course.update({
+      where: {
+        id: courseId,
+      },
+      data: {
+        exercises: {
+          connect: { id: exercise.id },
+        },
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Success",
+    });
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
 });
 
 export default exercise;
--- Backend/src/student.ts
+++ Backend/src/student.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.SECRETKEY || "Santosh";
 
 // Instantiate the client
 const prisma = new PrismaClient();
@@ -8,6 +11,141 @@ const student = Router();
 
 student.get("/", (req, res) => {
   res.send("Welcome to student endpoint");
+});
+
+student.post("/add-students", async (req: any, res: any) => {
+  try {
+    const token = req.body.token;
+    if (!token) {
+      return res.json({
+        success: false,
+        message: "Unauthorized Action, please login",
+      });
+    }
+
+    const verifyToken = jwt.verify(token, SECRET) as { email: string };
+    if (!verifyToken) {
+      return res.json({
+        success: false,
+        message:
+          "Cannot verify user, please login with appropriate credentials",
+      });
+    }
+
+    const { username, password, name } = req.body;
+
+    if (!username || !password || !name) {
+      return res.json({
+        success: false,
+        message: "All information related to students must be provided",
+      });
+    }
+
+    const courseId = req.body.courseId;
+    if (!courseId) {
+      return res.json({
+        success: false,
+        message: "Please select a course before adding students",
+      });
+    }
+
+    const getCourse = await prisma.course.findFirst({
+      where: {
+        id: courseId,
+        ownedBy: verifyToken.email,
+      },
+    });
+
+    if (!getCourse) {
+      return res.json({
+        success: false,
+        message: "Unauthorized Action, please try again",
+      });
+    }
+
+    await prisma.student.create({
+      data: {
+        username: username,
+        password: password,
+        name: name,
+        courseId: courseId,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message:
+        "A student login details has been added in the database successfully",
+    });
+    /*
+    model Student {
+  username String @unique
+  password String
+  name     String
+  courseId Int
+  course   Course @relation(fields: [courseId], references: [id])
+}
+  */
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
+});
+
+student.get("/login", async (req: any, res: any) => {
+  try {
+    const { username, password, courseId } = req.body;
+
+    if (!username || !password || !courseId) {
+      return res.json({
+        success: false,
+        message: "Please give all information correctly",
+      });
+    }
+
+    const searchUser = await prisma.student.findFirst({
+      where: {
+        courseId: courseId,
+        username: username,
+        password: password,
+      },
+    });
+
+    if (!searchUser) {
+      return res.json({
+        success: false,
+        message: "No such user found in this course :(",
+      });
+    }
+
+    //make a session cookie and send it to the user
+    //todo
+
+    return res.json({
+      success: true,
+      message: "Success",
+      cookie: "raanaoifuoiasudfoaijfdcaosiudfaoi",
+    });
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
+});
+
+student.get("./verify", (req: any, res: any) => {
+  try {
+    const cookie = req.body.cookie;
+    //will verift later and will do this part later
+  } catch (e: any) {
+    return res.json({
+      success: false,
+      message: e.message,
+    });
+  }
 });
 
 export default student;
